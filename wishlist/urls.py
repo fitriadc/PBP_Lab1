6a7dafbdae1eb2dde064c07d0b6d68f69cf20cd9
fitriadc <fitriadwicahya@gmail.com>
2022-09-22 13:42:31 +0700

    Completing Lab 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -6,6 +6,11 @@ from wishlist.views import data_show_json
 from wishlist.views import data_show_xml_by_id
 # sesuaikan dengan nama fungsi yang dibuat
 from wishlist.views import data_show_json_by_id
+from wishlist.views import register  # sesuaikan dengan nama fungsi yang dibuat
+# sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import login_user
+# sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
@@ -17,4 +22,10 @@ urlpatterns = [
          name='data_show_xml_by_id'),
     path('xml/<int:id>', data_show_json_by_id,
          name='data_show_json_by_id'),
+    # sesuaikan dengan nama fungsi yang dibuat
+    path('register/', register, name='register'),
+    # sesuaikan dengan nama fungsi yang dibuat
+    path('login/', login_user, name='login'),
+    # sesuaikan dengan nama fungsi yang dibuat
+    path('logout/', logout_user, name='logout'),
 ]
